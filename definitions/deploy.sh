fd8f7a2494389a484ab94a89375d30c3fcd4fcfc
truiz <admin@tonyruiz.altostrat.com>
2022-08-10 19:20:01 +0000

    added cloudbuild resources

---
+++ definitions/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ "${BRANCH_NAME}" != "main" ]]; then
+  export ENV=dev
+else
+  export ENV=prod
+fi
+
+echo "{\"projectId\": \"${PROJECT_ID}\", \"location\": \"US\"}" > .df-credentials.json
+cp "$dataform.json" dataform.json
+echo "copied $dataform.json"
+
+dataform install
+
+dataform run 
